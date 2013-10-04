7ecafeeea5e6f3f8f62eda97e3e0157f812fd524
chriskoo <chris.koo@163.com>
2013-10-04 23:04:46 +0800

    update pushdevice

--- classes/model/pushdevice.php
+++ classes/model/pushdevice.php
@@ -1,8 +1,34 @@
 <?php
 
+namespace MobilePush;
+
+use Orm\Model;
+
+class Model_PushDevice extends Model {
 
 
-class PushDevice {
+    protected static $_table_name = "push_device";
+    protected static $_write_connection = 'master_db';
+    protected static $_connection = 'slave_db';
+    protected static $_primary_key = array('id');
+
+    protected static $_properties = array(
+        'id',
+        'uid',
+        'title',
+        'content',
+        'images',
+        'comment_count',
+        'rate_count',
+        'createtime',
+        'updatetime',
+        'join_date',
+        'start_date',
+        'end_date',
+        'max_user_count',
+        'user_count',
+        'ordering',
+    );
 
 
 }
